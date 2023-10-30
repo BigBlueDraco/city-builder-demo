d252acbf0ace68b6973398a37f364438424a3dfb
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-10-30 20:50:10 +0200

    added teml of wfc

--- scripts/Level.cs
+++ scripts/Level.cs
@@ -1,7 +1,16 @@
 using System;
 using Godot;
 
-
+partial class Cell:Node3D
+{
+	public Boolean isCollapsed; 
+	public PackedScene[] variants;
+	public Cell(PackedScene[] variants)
+	{
+		this.variants= variants;
+		this.isCollapsed = false;
+	}
+}
 [Tool]
 public partial class Level : Node3D
 {
@@ -15,16 +24,31 @@ public partial class Level : Node3D
 
 	protected void Render()
 	{
-		Node3D[,] level = new Node3D[width, height];
-		
+		Cell[,] grid = new Cell[width, height];
 		for(int w = 0; w<= width-1; w++){
-		for(int h = 0; h<= height-1; h++){
-			Tile node  = (Tile)tiles[rand.Next(0, tiles.Length)].Instantiate();
-			node.Position = new Vector3(1+w*2,0,1+h*2);
-			AddChild(node);
-			node.RotateLeft(rand.Next(0,5));
-			
-			level[w,h] = node;
+		for(int h = 0; h<= height-1; h++)
+		{
+			grid[w,h] = new Cell(tiles);
+		}}
+		for(int w = 0; w<= width-1; w++){
+		for(int h = 0; h<= height-1; h++)
+		{
+
+			var elem = grid[w,h];
+			PackedScene[] variant = new PackedScene[1];
+			Tile node = (Tile)elem.variants[0].Instantiate();
+			// GD.Print(elem.variants[0].right);
+			var child = node.GetChildren();
+			GD.Print(child);
+			// elem.variants[rand.Next(0, elem.variants.Length)].Instantiate();
+			elem.isCollapsed = true;
+			// Tile node = elem.variants[rand.Next(0, elem.variants.Length)];
+			// node.Position = new Vector3(1+w*2,0,1+h*2);
+			// AddChild(elem.variants[rand.Next(0, elem.variants.Length)]);
+			// grid[w+1, h].variants = node.right;
+			// grid[w-1, h].variants = node.left;
+			// grid[w, h-1].variants = node.down;
+			// grid[w, h+1].variants = node.up;
 		}}
 	}
 	public override void _Ready()
--- scripts/Tile.cs
+++ scripts/Tile.cs
@@ -8,13 +8,13 @@ public partial class Tile : Node3D
 	private Node3D node;
 	private int rotationPosition; 
 	[Export]
-	private PackedScene[] up = new PackedScene[0];
+	public PackedScene[] up = new PackedScene[0];
 	[Export]
-	private PackedScene[] left = new PackedScene[0];
+	public PackedScene[] left = new PackedScene[0];
 	[Export]
-	private PackedScene[] right = new PackedScene[0];
+	public PackedScene[] right = new PackedScene[0];
 	[Export]
-	private PackedScene[] down = new PackedScene[0];
+	public PackedScene[] down = new PackedScene[0];
 	
 	public PackedScene[] GetUp()
 	{
@@ -29,7 +29,6 @@ public partial class Tile : Node3D
 	[Export]
 	private PackedScene[] variants = new PackedScene[1];
 	protected void Render(){
-		GD.Print(this);
 		Node3D node  = (Node3D)variants[rand.Next(0, variants.Length)].Instantiate();
 		AddChild(node);
 		this.node = node;
@@ -37,7 +36,6 @@ public partial class Tile : Node3D
 	public void RotateLeft(int times = 1)
 	{
 		this.rotationPosition =((this.rotationPosition+times)%4);
-		GD.Print((this.rotationPosition+times)%4);
 		float rad = (float)((this.rotationPosition  * 90) / (180 / Math.PI));
 		this.node.RotateY(rad);
 	}
@@ -46,7 +44,6 @@ public partial class Tile : Node3D
 		this.rotationPosition =((this.rotationPosition+times)%4);
 		GD.Print((this.rotationPosition+times)%4);
 		float rad = (float)((this.rotationPosition  * 90) / (180 / Math.PI));
-		this.node.RotateY(rad);
 	}
 	public override void _Ready()
 	{
